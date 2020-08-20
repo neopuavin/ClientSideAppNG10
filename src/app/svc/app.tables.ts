1859a6845695e4e80527ab305ce83242e978ae2d
sogatechgit <izyte@yahoo.com>
2020-08-20 11:42:07 +0800

    after revision of site banner

--- src/app/svc/app.tables.ts
+++ src/app/svc/app.tables.ts
@@ -1,5 +1,5 @@
 /***********************************************************************
-* Automatically generated on 8/19/2020 8:53:36 AM
+* Automatically generated on 8/20/2020 9:15:36 AM
 ***********************************************************************/
 
 import { AppCommonMethodsService } from '../api/svc/app-common-methods.service';
